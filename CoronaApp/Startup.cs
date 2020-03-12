f3677f3fadfff8ca3bd8791406b05427b5274d67
Hannes Preishuber <hannesp@ppedv.de>
2020-03-12 16:19:47 +0100

    DI Viewmodel

--- CoronaApp/Startup.cs
+++ CoronaApp/Startup.cs
@@ -29,6 +29,7 @@ namespace CoronaApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ToDoVM>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
