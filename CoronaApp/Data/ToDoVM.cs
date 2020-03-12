319f45a3995930db03549004cccb8a43e92f3d26
Hannes Preishuber <hannesp@ppedv.de>
2020-03-12 16:26:42 +0100

    Viewmodel mit Propertychanged Event

--- CoronaApp/Data/ToDoVM.cs
+++ CoronaApp/Data/ToDoVM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Server.IIS.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace CoronaApp.Data
 {
     public class ToDoVM
     {
+        public event EventHandler PropertyChangedEvent;
+
+
         public Aufgabe AktuellAufgabe { get; set; } = new Aufgabe();
         public List<Aufgabe> Aufgaben { get; set; } = new List<Aufgabe>();
 
@@ -20,7 +24,7 @@ namespace CoronaApp.Data
                 ID = Aufgaben.Count
             });
 
-
+            PropertyChangedEvent.Invoke(this, EventArgs.Empty);
         }
 
     }
