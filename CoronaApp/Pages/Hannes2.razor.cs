0cad182a13f6f6ecab9f7d6ceb200cf9ee6a7d3d
Hannes Preishuber <hannesp@ppedv.de>
2020-03-12 11:00:23 +0100

    Projektdateien hinzufügen.

---
+++ CoronaApp/Pages/Hannes2.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaApp.Pages
+{
+    public partial class Hannes2
+    {
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
