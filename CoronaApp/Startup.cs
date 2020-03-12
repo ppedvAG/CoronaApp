a72c0252a8b6afa2d4707256b537ee269bdf630a
Hannes Preishuber <hannesp@ppedv.de>
2020-03-12 21:10:50 +0100

    Input Binding

--- CoronaApp/Startup.cs
+++ CoronaApp/Startup.cs
@@ -29,7 +29,7 @@ namespace CoronaApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<ToDoVM>();
+            services.AddScoped<ToDoVM>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
