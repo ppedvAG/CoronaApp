f27662a462cd5050d1794fc941fa85d773a4127e
Hannes Preishuber <hannesp@ppedv.de>
2020-03-13 11:26:34 +0100

    Annotations

--- CoronaApp/Data/Aufgaben.cs
+++ CoronaApp/Data/Aufgaben.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace CoronaApp.Data
 {
     public class Aufgabe
     {
+
         public int ID { get; set; }
+        [Required(ErrorMessage ="da muss was rein")]
+        [MinLength(5,ErrorMessage ="mind 5 Zeichen")]
+        [MaxLength(20,ErrorMessage="max 20 "]
         public string Text { get; set; }
+
+        [Zukunft(ErrorMessage ="muss in Zukunft leigen")]
         public DateTime ErledigtBis { get; set; }
         public Boolean Erledigt { get; set; }
 
---
+++ CoronaApp/Data/ZukunftAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaApp.Data
+{
+    public class ZukunftAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var datum = (DateTime)value;
+            if (datum > DateTime.Now)
+            {
+
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
