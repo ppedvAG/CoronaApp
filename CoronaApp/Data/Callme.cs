0d665ca8165eb63efd014af9a12766990a33af9d
Hannes Preishuber <hannesp@ppedv.de>
2020-03-13 14:36:38 +0100

    JavaScript INterop

---
+++ CoronaApp/Data/Callme.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaApp.Data
+{
+    public class Callme
+    {
+        [JSInvokable]
+        public static void CallHannes()
+        {
+
+        }
+    }
+}
