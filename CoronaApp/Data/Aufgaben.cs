75d8a58b7c467ead2e093f6a8ec6e74314ec0052
Hannes Preishuber <hannesp@ppedv.de>
2020-03-13 10:46:03 +0100

    Todo LocalStorage, Edit, Erledigen

--- CoronaApp/Data/Aufgaben.cs
+++ CoronaApp/Data/Aufgaben.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CoronaApp.Data
@@ -11,6 +12,8 @@ namespace CoronaApp.Data
         public string Text { get; set; }
         public DateTime ErledigtBis { get; set; }
         public Boolean Erledigt { get; set; }
+
+   [JsonIgnore]
         public Boolean Bearbeiten { get; set; }
     }
 }
